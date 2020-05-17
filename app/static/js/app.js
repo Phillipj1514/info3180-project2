bd68f434644158fb03b3aaf0540c25e0b084b826
Lateefah Smellie <lateefah.smellie@gmail.com>
2020-05-17 14:08:10 -0500

    add logo image and fonts, update header structure and styles, complete skeleton of components

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,8 +1,9 @@
 /* Add your Application JavaScript */
 Vue.component('app-header', {
     template: `
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-      <a class="navbar-brand" href="#">Project 2</a>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+        <img src='static/images/photogram.png' class="header-logo" alt="Photogram Logo" />
+      <a class="navbar-brand" href="#">Photogram</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -11,7 +12,16 @@ Vue.component('app-header', {
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
             <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
-          </li>
+          </li>  
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/explore">Explore <span class="sr-only">(current)</span></router-link>
+          </li>  
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/my_profile">My Profile<span class="sr-only">(current)</span></router-link>
+          </li>  
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/logout">Logout<span class="sr-only">(current)</span></router-link>
+          </li>  
         </ul>
       </div>
     </nav>
@@ -30,9 +40,23 @@ Vue.component('app-footer', {
 
 const Home = Vue.component('home', {
    template: `
-    <div class="jumbotron">
-        <h1>Project 2</h1>
-        <p class="lead">The start of something big</p>
+    <div class="row landing-container">
+        <div class="col-md-4 landing-container-child">
+            <img src="/static/images/photogram.png" id="landing-img"/>
+        </div>
+        <div class="col-md-4  landing-container-child float-clear">
+          <div class="card">
+            <img class="card-img-top" src="static/images/photogram.png" alt="Card image cap">
+            <div class="card-body">
+              <hr>
+              <p class="card-text">Share photos of your favourite moments with friends, family and the world.</p>
+              <div >
+                  <router-link class="btn btn-success col-md-5" to="/register">Register</router-link>
+                  <router-link class="btn btn-primary col-md-5" to="/login">Login</router-link>
+              </div>
+            </div>
+          </div>  
+        </div>
     </div>
    `,
     data: function() {
@@ -56,46 +80,54 @@ const Register = Vue.component('register', {
     template: `
     <div>
         <h1>Register</h1>
-        <div class="register-form-container">
-           <form enctype="multipart/form-data" action="/register" method="POST">
+        <div class="auth-form-container">
+           <form enctype="multipart/form-data" action="/register" method="POST" class="col-md-12">
             <div class="form-group">
-                <div class="form-label">Username</div>
-                <input type="text" class="form-control"></input>
-            </div>
-            <div class="form-group">
-                <div class="form-label">Password</div>
-                <input type="text" class="form-control" placeholder="Username"></input>
+                <label class="form-label" for="usernameField">Username</label>
+                <input type="text" class="form-control" id="usernameField" placeholder="Enter username" required></input>
+                <div class="invalid-feedback">This field is required</div>
             </div>
             <div class="form-group">
-                <div class="form-label">Confirm Password</div>
-                <input type="text" class="form-control" placeholder="Username"></input>
+                <label class="form-label" for="passwordField">Password</label>
+                <input type="password" class="form-control" id = "passwordField" placeholder="Enter password" required></input>
+                <div class="invalid-feedback">This field is required</div>
             </div>
             <div class="form-group">
-                <div class="form-label">Firstname</div>
-                <input type="text" class="form-control" placeholder="Username"></input>
+                <label class="form-label" for="confirmPasswordField">Confirm Password</label>
+                <input type="password" id="confirmPasswordField" class="form-control" placeholder="Enter password again" required>
+                <div class="invalid-feedback">This field is required</div>
             </div>
             <div class="form-group">
-                <div class="form-label">Lastname</div>
-                <input type="text" class="form-control" placeholder="Username"></input>
+                <label class="form-label" for="firstnameField">Firstname</label>
+                <input type="text" class="form-control" placeholder="Enter firstname" id="firstnameField" required></input>
+                <div class="invalid-feedback">This field is required</div>
             </div>
             <div class="form-group">
-                <div class="form-label">Email</div>
-                <input type="text" class="form-control" placeholder="Username"></input>
+                <label class="form-label" for="lastnameField">Lastname</label>
+                <input type="text" id="lastnameField" class="form-control" placeholder="Enter lastname" required></input>
+                <div class="invalid-feedback">This field is required</div>
             </div>
             <div class="form-group">
-                <div class="form-label">Location</div>
-                <input type="text" class="form-control" placeholder="Username"></input>
+                <label class="form-label" for="emailField">Email</label>
+                <input type="email" class="form-control" placeholder="Enter email" id="emailField" required></input>
+                <div class="invalid-feedback">This field is required</div>
             </div>
             <div class="form-group">
-                <div class="form-label">Biography</div>
-                <input type="text" class="form-control" placeholder="Username"></input>
+                <label class="form-label" for="locationField">Location</label>
+                <input type="text" id="locationField" class="form-control" placeholder="Where are you from?" required></input>
+                <div class="invalid-feedback">This field is required</div>
             </div>
-                <div class="form-group">
-                    <div class="form-label">Photo</div>
-                    <div class="file-input-container">
-                        <input type="file" >Browse</input>
-                    </div>
+            <div class="form-group">
+                <label class="form-label" for="biographyField">Biography</label>
+                <textarea class="form-control" placeholder="Tell us a little about you." id="biographyField" rows="4" required></textarea>
+                <div class="invalid-feedback">This field is required</div>
             </div>
+            <div class="form-group d-flex flex-column">
+                <label class="form-label" for="photoField">Photo</label>
+                <input type="file" id="photoField" required></input>
+                <div class="invalid-feedback">This field is required</div>
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
         </form>    
     </div>
     `,
@@ -107,8 +139,24 @@ const Register = Vue.component('register', {
 
 const Login = Vue.component('login', {
     template: `
-    <div>
-        <h1>lOGIN pAGE</h1>
+        <div>
+            <h1>Login</h1>
+            <div class="auth-form-container">
+                <form class="" action="" method="POST">
+                    <div class="form-group">
+                        <label class="form-label" for="usernameField">Username</label>
+                        <input type="text" id="usernameField" required></input>
+                        <div class="invalid-feedback">This field is required.</div>
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label" for="passwordField">Password</label>
+                        <input type="password" id="pasdwordField" required></input>
+                        <div class="invalid-feedback">This field is required.</div>
+                    </div>
+                    <br/>
+                    <button type="submit" class="btn btn-primary" id="loginBtn">Login</button>
+                </form>
+        </div>
     </div>
     `,
     data: function () {
@@ -118,10 +166,29 @@ const Login = Vue.component('login', {
 
 const Feed = Vue.component('feed', {
     template: `
-    <div>
-        <h1>Explore</h1>
+    <div class="container">
+        <div class="feed-posts">
+            <div class="post card">
+                <div class="post-header">
+                    <img src='static/images/photogram.png' height="30" width="30" />
+                    <div class="poster"></div>                        
+                </div>
+                <img class="post-image" src='static/images/uploads/2.jpg' width="500" height="500"/>
+                <div class='post-caption'>I was once a stranger walking through this city. I wmanaged to dlkdlksdlkdlklkdlk lorem impsum dloeremand fk lkdj kja j;kd djlk ddkljdsljdsijepi ddjhdoirj fj</div>
+                <div class="post-footer">
+                    <div class="footer-left">
+                        <img src='static/images/photogram.png' width="20" height="20" class="like-icon"/>
+                        <p class="like-details">10 Likes</p>
+                    <div class="footer-left">24 Apr 2018</div>
+                </div>
+
+            </div>                
+        </div>
+        <div class="sidebar">
+             <button type="submit" class="btn btn-primary new-post-btn">New Post</button>   
+        </div>
     </div>
-    `,
+   `,
     data: function () {
         return {}
     }
@@ -129,9 +196,23 @@ const Feed = Vue.component('feed', {
 
 const CreatePost = Vue.component('post', {
     template: `
-    <div>
-        <h1>cREATE A NEW POST</h1>
-    </div>
+        <div>
+            <h1>New Post</h1>
+            <div class="auth-form-container">
+                <form class="" action="" method="POST">
+                    <div class="form-group d-flex flex-column">
+                        <label class="form-label" for="photoField">Photo</label>
+                        <input type="file" id="photoField" required></input>
+                        <div class="invalid-feedback">This field is required</div>
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label" for="captionField">Caption</label>
+                        <textarea class="form-control" placeholder="Tell us a little about this picture." id="captionField" rows="4"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary" id="loginBtn">Sumbit</button>
+                </form>
+            </div>
+        </div>
     `,
     data: function () {
         return {}
@@ -140,13 +221,52 @@ const CreatePost = Vue.component('post', {
 
 const UserProfile = Vue.component('user-profile', {
     template: `
-    <div>
-        <h1>View user profile</h1>
-    </div>
+    <div class="full-page-container">
+        <div class="profile-head-cotainer">
+            <img src='static/uploads/2.jpg' alt='Profile Photo' style="width:100px; height:100px"/> 
+            <div class="profile-details-container">
+                <div class="title">Rosa Diaz</div>
+                <div class="location">Kingston, Jamaica</div>
+                <div class="member-details">Member since January 2018</div>
+                <div class="biography-details">THis is my short biography so you can learn more about me</div>
+            </div>
+            <div class="engagement-container">
+                <div class="stats">
+                    <div class="stat">
+                        <div class="stat-value">6</div>
+                        <div class="stat-label">Posts</div>
+                    </div>
+                    <div class="stat">
+                        <div class="stat-value">10</div>
+                        <div class="stat-label">Followers</div>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary follow-btn">Follow</button>
+            </div>
+        </div>
+        <div class="posts-container col-md-3">
+            <div class="no-posts">This user hasn't posted anything yet.</div>
+                <img src='static/images/uploads/2.jpg' width="250" height="250" />   
+                <img src='static/images/uploads/2.jpg' width="250" height="250" /> 
+                <img src='static/images/uploads/2.jpg' width="250" height="250" />
+                <img src='static/images/uploads/2.jpg' width="250" height="250" />
+                <img src='static/images/uploads/2.jpg' width="250" height="250" />
+                <img src='static/images/uploads/2.jpg' width="250" height="250"/>
+        </div>
+        </div>
     `,
     data: function () {
-        return {}
-    }
+        return {
+            profilePhotoSource: require("../imauploads/2.jpg")
+        }
+    },
+    methods: {
+        getProfilePhotoUrl() {
+            var imageUrl = require("static/images/uploads/2.jpg")
+            return imageUrl;
+        }
+    },
+    props: ['profilePhotoSource']
 })
 
 
@@ -159,7 +279,7 @@ const router = new VueRouter({
         { path: "/login", component: Login },
         { path: "/logout", component: Home },
         { path: "/explore", component: Feed },
-        { path: "/users/{user_id}", component: UserProfile },
+        { path: "/users/", component: UserProfile },
         { path: "/posts/new", component: CreatePost },
         {path: "*", component: NotFound }
     ]
@@ -174,157 +294,3 @@ let app = new Vue({
         flash: false
     }
 });
-
-////me
-//fgf
-/* Add your Application JavaScript 
-Vue.component('app-header', {
-    template: `
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-              <a class="navbar-brand" href="#">VueJS App</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                  <li class="nav-item active">
-                    <router-link to="/" class="nav-link">Home</router-link>
-                  </li>
-                  <li class="nav-item">
-                    <router-link to="/news" class="nav-link">News</router-link>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-        </header>    
-    `,
-    data: function() {
-      return {};
-    }
-});
-
-Vue.component('app-footer', {
-    template: `
-        <footer>
-            <div class="container">
-                <p>Copyright &copy {{ year }} Flask Inc.</p>
-            </div>
-        </footer>
-    `,
-    data: function() {
-        return {
-            year: (new Date).getFullYear()
-        }
-    }
-})
-
-const NewsList = Vue.component('news-list',{
-  template:`
-      <div class="news">
-        <h2 class = "text-center">News</h2>
-        <div class="form-inline d-flex justify-content-center">
-          <div class="form-group mx-sm-3 mb-2">
-            <label class="sr-only" for="search">Search</label>
-            <input type="search" name="search" v-model="searchTerm"id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
-            <p>You are searching for <b>{{ searchTerm }}</b></p>
-          </div>
-        </div>
-        <div class = "d-flex justify-content-center">
-          <button class="btn btn-primary mb-2 searchbtn" @click="searchNews">Search</button>
-        </div>
-        <div class="news__list d-flex align-content-start justify-content-center flex-wrap">
-          <!-- <news-item v-for="news in news_items"> {{ news.name}}</news-item> -->
-          <!--  <li v-for="article in articles"  class="news__item">{{ article.title }}</li> -->
-          <div  v-for="article in articles" class="news__item  article ">
-          <div class = "tstrip  bg-primary border-top"></div>
-            <div class = "container border-bottom border-right border-left">
-              <h5>{{ article.title }}</h5>
-              <div class = "text-center">  
-                <img class= "img-fluid rounded":src="article.urlToImage">
-              </div>
-              <p>{{ article.description }}</p>
-            </div>
-          </div>
-         
-        </div>
-      </div>
-  `,
-
-  created(){
-    let self = this;
-    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=<key>')
-    .then(function(response) {
-      return response.json();
-    }).then(function(data) {
-      console.log(data);
-      self.articles = data.articles;
-    });
-  },
-  
-  data(){
-    return{
-      news_items:[
-        {name:'News item 1'},
-        {name:'News item 2'},
-        {name:'News item 3'}
-      ],
-      articles: [],
-      searchTerm: '' 
-    }
-  },
-
-  methods: {
-    searchNews: function() {
-      let self = this;
-      fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + '&language=en&apiKey=<key>')
-      .then(function(response) {
-        return response.json();
-      }).then(function(data) {
-        console.log(data);
-        self.articles = data.articles;
-      });
-    }
-  } 
-
-});
-
-Vue.component('news-item',{
-  template:`
-      <li class="news__item"><slot></slot></li> 
-  `,
-});
-
-const Home = Vue.component('home', {
-  template: `
-        <div class="home">
-        <img src="/static/images/logo.png" alt="VueJS Logo">
-        <h1>{{ welcome }}</h1>
-        </div>
-  `,
-  data(){
-    return {
-      welcome: 'Hello World! Welcome to VueJS'
-    }
-  }
- });
-
-const router = new VueRouter({
-  mode: 'history',
-  routes: [
-    { path: '/', component: Home },
-    { path: '/register', component: Register }
-    { path: '/login', component: Login }
-    { path: '/', component: NewsList }
-    { path: '/news', component: NewsList }
-    { path: '/news', component: NewsList }
-    { path: '/news', component: NewsList }
-  ]
-});
-
-let app = new Vue({
-    el: '#app',
-    router
-});*/
-
